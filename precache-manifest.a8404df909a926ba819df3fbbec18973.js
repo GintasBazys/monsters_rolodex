193195bd02bb3dd46a831c96b247f972a8afc24d
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-22 17:35:08 +0300

    Updates

---
+++ precache-manifest.a8404df909a926ba819df3fbbec18973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b45b735e0cb0c2253bb83e7ec7fd470",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "ef17853067c4d53d9c60",
+    "url": "/monsters_rolodex/static/css/main.46774053.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters_rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef17853067c4d53d9c60",
+    "url": "/monsters_rolodex/static/js/main.afd5fd71.chunk.js"
+  },
+  {
+    "revision": "c45966b3fc49e39bb793",
+    "url": "/monsters_rolodex/static/js/runtime-main.b97009c0.js"
+  }
+]);
